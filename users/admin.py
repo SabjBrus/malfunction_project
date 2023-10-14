7bd4c0a985ad85688c7463f53ecc503f15fa9205
Борис Жуков <boris10264@gmail.com>
2023-10-14 19:49:26 +0300

    register django admin for users and departments

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import CustomUser, Department
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'email',
+        'first_name',
+        'last_name',
+        'username',
+        'date_joined',
+        'department',
+    )
+    list_filter = ('username', 'email')
+    search_fields = ('username', 'email')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    list_filter = ('name', 'slug')
+    search_fields = ('name', 'slug')
+    empty_value_display = '-пусто-'
