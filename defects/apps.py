bb2e68dc048a5042c9a93de1f6042b7d399787aa
Борис Жуков <boris10264@gmail.com>
2023-10-21 18:37:57 +0300

    flake8, pymarkdown changes

--- defects/apps.py
+++ defects/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DefectsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "defects"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'defects'
