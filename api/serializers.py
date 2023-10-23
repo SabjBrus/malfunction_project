d6723ab7c1cfeddf0f26cb5c0a8a6b066312bdf7
Борис Жуков <boris10264@gmail.com>
2023-10-23 20:39:53 +0300

    add api for departments

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from djoser.serializers import UserCreateSerializer, UserSerializer
+from rest_framework import serializers
 
-from users.models import CustomUser
+from users.models import CustomUser, Department
 
 
 class CustomUserCreateSerializer(UserCreateSerializer):
@@ -13,3 +14,9 @@ class CustomUserSerializer(UserSerializer):
     class Meta:
         model = CustomUser
         fields = ('id', 'first_name', 'last_name', 'slug', 'email')
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = ('id', 'name', 'slug')
--- api/views.py
+++ api/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from api.serializers import CustomUserSerializer
-from users.models import CustomUser
+from api.serializers import CustomUserSerializer, DepartmentSerializer
+from users.models import CustomUser, Department
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
