9eabd41d82e1584790cba25c14463570b167216e
Борис Жуков <boris10264@gmail.com>
2023-10-29 20:21:25 +0300

    add django filter and filter fields at DefectViewSet

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from api.serializers import CustomUserSerializer, DepartmentSerializer, DefectSerializer
 from defects.models import Defect
@@ -22,3 +23,5 @@ class DefectViewSet(viewsets.ModelViewSet):
     serializer_class = DefectSerializer
     permission_classes = (OwnerOrReadOnly,)
     pagination_class = DefectsPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('priority', 'status')
