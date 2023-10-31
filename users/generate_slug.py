f531b7c626704a95ffc389e8b41ad6399a7bf68f
Борис Жуков <boris10264@gmail.com>
2023-10-31 21:23:23 +0300

    add flake8 changes

--- users/generate_slug.py
+++ users/generate_slug.py
@@ -7,5 +7,4 @@ def generate_slug(first_name, last_name):
     full_name = f'{first_name} {last_name}'
     slug = unidecode(full_name)
     slug = re.sub(r'[^\w\s-]', '', slug).strip()
-    slug = re.sub(r'[-\s]+', '-', slug).lower()
-    return slug
+    return re.sub(r'[-\s]+', '-', slug).lower()
